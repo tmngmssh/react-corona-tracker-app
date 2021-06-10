90b78568cb9cf8832bbe0f61aa7f1553ca6d1bcd
tmngmssh <tominaga.0611@gmail.com>
2021-06-10 19:14:28 +0900

    finished chapter5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import WorldPage from "./pages/WorldPage";
 import './App.css';
 
 function App() {
-  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [country, setCountry] = useState("japan");
   const [countryData, setCountryData] = useState({
     date: "",
     newConfirmed: "",
@@ -16,30 +17,37 @@ function App() {
   });
   const [allCountriesData, setAllCountriesData] = useState([]);
 
-  const getCountryData = () => {
-    fetch(`https://api.covid19api.com/country/${country}`)
-    .then(res => res.json())
-    .then(data => {
-      setCountryData({
-        date: data[data.length -1].Date,
-        newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
-        totalConfirmed: data[data.length -1].Confirmed,
-        newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
-        totalRecovered: data[data.length -1].Recovered
-      });
-    })
-  }
+  useEffect(() => {
+    const getCountryData = () => {
+      setLoading(true);
+      fetch(`https://api.covid19api.com/country/${country}`)
+      .then(res => res.json())
+      .then(data => {
+        setCountryData({
+          date: data[data.length -1].Date,
+          newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
+          totalConfirmed: data[data.length -1].Confirmed,
+          newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
+          totalRecovered: data[data.length -1].Recovered
+        });
+        setLoading(false);
+      })
+      .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"));
+    }
+    getCountryData();
+  },[country])
 
   useEffect(() => {
     fetch("https://api.covid19api.com/summary")
     .then(res => res.json())
     .then(data => setAllCountriesData(data.Countries))
+    .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"));
   }, []);
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} countryData={countryData}/>
+          <TopPage countriesJson={countriesJson} setCountry={setCountry} countryData={countryData} loading={loading}/>
         </Route>
         <Route exact path="/world">
           <WorldPage allCountriesData={allCountriesData} />
